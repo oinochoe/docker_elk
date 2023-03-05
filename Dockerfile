370d8a0f73e7f8712c8ede44da1ff96b40f30703
YeongminKim <oinochoe@naver.com>
2023-03-05 19:57:42 +0900

    elk and docker example

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+﻿# Use the official Node.js image as the base image
+FROM node:latest
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the package.json and package-lock.json files to the container
+COPY package*.json ./
+
+# Install the app dependencies
+RUN npm install
+
+# Copy the rest of the app files to the container
+COPY . .
+
+# Set the environment variables for ElasticSearch
+ENV ELASTICSEARCH_HOST=elasticsearch
+ENV ELASTICSEARCH_PORT=9200
+
+# Expose the port that the app will run on
+EXPOSE 3000
+
+# Start the app
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,43 @@
+﻿const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("server connected");
+});
+
+app.get("/search", async (req, res) => {
+  const query = req.query.q;
+
+  try {
+    const results = await searchQuery(query);
+    res.json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal sever error" });
+  }
+});
+
+const { Client } = require("elasticsearch");
+
+const client = new Client({
+  host: "http://host.docker.internal:9200",
+  log: "trace",
+});
+
+const searchQuery = async function (query) {
+  const { hits } = await client.search({
+    index: "indexing",
+    body: {
+      query: {
+        fuzzy: {
+          content: {
+            value: query,
+            fuzziness: "AUTO",
+          },
+        },
+      },
+    },
+  });
+  return hits.hits ?? [];
+};
